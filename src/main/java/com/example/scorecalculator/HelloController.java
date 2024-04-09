f4a478c850e0c88cebe973549f359b26abdd48ea
mingshengWin13 <ming.chai@ucdconnect.ie>
2024-04-09 22:25:52 +0100

    gui layout implemented

--- src/main/java/com/example/scorecalculator/HelloController.java
+++ src/main/java/com/example/scorecalculator/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.scorecalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+//    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
+    private String str;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField baseScore;
+
+    @FXML
+    private Button deleteLast;
+
+    @FXML
+    protected void onDeleteButtonClick(ActionEvent event)
+    {
+        baseScore.setText(str);
     }
+
+    public HelloController()
+    {
+        str = "this";
+    }
+
 }

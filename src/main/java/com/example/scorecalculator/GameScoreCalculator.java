1d5ffe36ba6d11de3991c6b0cbc9e1b423e81ecd
Naux334 <xinyuan.he334@gmail.com>
2024-04-09 22:43:04 +0100

    Add Emergency task to calculator, add test case in main

--- src/main/java/com/example/scorecalculator/GameScoreCalculator.java
+++ src/main/java/com/example/scorecalculator/GameScoreCalculator.java
@@ -1,6 +1,26 @@
 package com.example.scorecalculator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameScoreCalculator {
+
+    private int baseScore;
+    private int collectionScore;
+    private int cipherBoardScore;
+    private Map<EmergencyTask, Integer> emergencyTaskScore;
+    private int endingScore;
+    private int debtPenalty;
+
+
+    public GameScoreCalculator(int baseScore, int collectionScore, int cipherBoardScore, int endingScore, int debtPenalty) {
+        this.baseScore = baseScore;
+        this.collectionScore = collectionScore * 10;
+        this.cipherBoardScore = cipherBoardScore * 5;
+        this.emergencyTaskScore = new HashMap<>();
+        this.endingScore = (endingScore - 100) * 50;
+        this.debtPenalty = debtPenalty;
+    }
+
     public void setBaseScore(int baseScore) {
         this.baseScore = baseScore;
     }
@@ -21,25 +41,12 @@ public class GameScoreCalculator {
         this.debtPenalty = debtPenalty;
     }
 
-    private int baseScore;
-    private int collectionScore;
-    private int cipherBoardScore;
-    private int emergencyTaskScore;
-    private int endingScore;
-    private int debtPenalty;
-
-
-    public GameScoreCalculator(int baseScore, int collectionScore, int cipherBoardScore, int emergencyTaskScore, int endingScore, int debtPenalty) {
-        this.baseScore = baseScore;
-        this.collectionScore = collectionScore * 10;
-        this.cipherBoardScore = cipherBoardScore * 5;
-        this.emergencyTaskScore = emergencyTaskScore;
-        this.endingScore = (endingScore - 100) * 50;
-        this.debtPenalty = debtPenalty;
+    public void addEmergencyTask(EmergencyTask task) {
+        emergencyTaskScore.put(task, emergencyTaskScore.getOrDefault(task, 0) + task.getScore());
     }
 
     public int calculateTotalScore() {
-        int totalScore = baseScore + collectionScore + cipherBoardScore + endingScore - debtPenalty;
-        return totalScore;
+        int totalEmergencyScore = emergencyTaskScore.values().stream().mapToInt(Integer::intValue).sum();
+        return totalEmergencyScore + baseScore + collectionScore + cipherBoardScore + endingScore - debtPenalty;
     }
 }
--- src/main/java/com/example/scorecalculator/Main.java
+++ src/main/java/com/example/scorecalculator/Main.java
@@ -2,17 +2,13 @@ package com.example.scorecalculator;
 
 public class Main {
     public static void main(String[] args) {
+        GameScoreCalculator calculator = new GameScoreCalculator(100, 3, 2, 500,0);
 
-        EmergencyTask task1 = EmergencyTask.ICE_SHADOW;
-        EmergencyTask task2 = EmergencyTask.NETWORK;
-        EmergencyTask task3 = EmergencyTask.COMPANY_CONFLICT;
-        EmergencyTask task4 = EmergencyTask.ICE_SHADOW;
+        calculator.addEmergencyTask(EmergencyTask.ICE_SHADOW);
+        calculator.addEmergencyTask(EmergencyTask.NETWORK);
+        calculator.addEmergencyTask(EmergencyTask.ICE_SHADOW);
 
-
-
-        System.out.println("Task 1: " + task1.getName() + ", Score: " + task1.getScore());
-        System.out.println("Task 2: " + task2.getName() + ", Score: " + task2.getScore());
-        System.out.println("Task 3: " + task3.getName() + ", Score: " + task3.getScore());
-        System.out.println("Task 3: " + task3.getName() + ", Score: " + task4.getScore());
+        int totalScore = calculator.calculateTotalScore();
+        System.out.println("Total Score: " + totalScore);
     }
 }

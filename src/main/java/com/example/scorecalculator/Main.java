788b2057be0c0e055ad3bd288920d19859b49507
mingshengWin13 <ming.chai@ucdconnect.ie>
2024-04-10 13:02:07 +0100

    Jar added

--- src/main/java/com/example/scorecalculator/Main.java
+++ src/main/java/com/example/scorecalculator/Main.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        GameScoreCalculator calculator = new GameScoreCalculator(100, 3, 2, 500,0);
-
-        calculator.addEmergencyTask(EmergencyTask.ICE_SHADOW.getScore());
-        calculator.addEmergencyTask(EmergencyTask.NETWORK.getScore());
-        calculator.addEmergencyTask(EmergencyTask.ICE_SHADOW.getScore());
-        int totalScore = calculator.calculateTotalScore();
-        System.out.println("Total Score: " + totalScore);
+        HelloApplication.main(args);
+//        GameScoreCalculator calculator = new GameScoreCalculator(100, 3, 2, 500,0);
+//
+//        calculator.addEmergencyTask(EmergencyTask.ICE_SHADOW.getScore());
+//        calculator.addEmergencyTask(EmergencyTask.NETWORK.getScore());
+//        calculator.addEmergencyTask(EmergencyTask.ICE_SHADOW.getScore());
+//        int totalScore = calculator.calculateTotalScore();
+//        System.out.println("Total Score: " + totalScore);
     }
 }
